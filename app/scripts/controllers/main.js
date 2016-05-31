21edcb229178e4689adc49f2c7f9a975c68f0b2f
efloden <earl.mark.27@gmail.com>
2016-05-31 11:05:58 +1000

    Auto Generate button loading JSON data implemented

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,15 @@
  * Controller of the meikaiAppApp
  */
 angular.module('meikaiAppApp')
-  .controller('MainCtrl', function ($scope, localStorageService) {
+  .controller('MainCtrl', function ($scope, $http, localStorageService) {
+    var samplecards = [];
+    $http.get('samplecards.json').success(function (data){
+        samplecards = data;
+    });
     var kanjisInStore = localStorageService.get('kanjis');
-
+    $scope.correct = 0;
+    $scope.incorrect = 0;
+    $scope.attempts = 0;
     $scope.kanjis = kanjisInStore || [];
 
     $scope.$watch('kanjis', function () {
@@ -24,4 +30,22 @@ angular.module('meikaiAppApp')
     $scope.removeKanji = function (index) {
       $scope.kanjis.splice(index, 1);
     };
+
+    $scope.checkKanji = function(kindex, lindex) {
+      if (kindex == lindex) {
+        $scope.correct++;
+      } else {
+        $scope.incorrect++;
+      }
+      $scope.attempts++;
+    }
+    $scope.generatejson = function() {
+      var log = [];
+      angular.forEach(samplecards, function(value, key) {
+        $scope.kanjis.push(value.kanji + ' ' + value.reading + ' ' + value.meaning);
+      }, log)
+      $scope.kanji = '';
+    }
+
+
   });
